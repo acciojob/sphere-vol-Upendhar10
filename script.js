7c384ea4a1bf2ec074cc6874f9b025af2507f1f3
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-09 23:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let radius = document.getElementById('radius').innerText;
+let volume = document.getElemntById('volume');
+
 function volume_sphere() {
     //Write your code here
+	// formula => (4/3)*(3.14)*r*r*r;
+
+	radius = parseInt(radius);
+	
+	if(radius < 0 || isNaN(radius)) return NaN;
+
+	let volum = 4/3 * (3.14) * (radius * radius * radius);
+
+	volume.innerText = volum.toFixed(4);
   
 } 
 
